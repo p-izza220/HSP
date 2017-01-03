f608658858e3afb6e610cc2e3342f832e71cca5d
Izza <icg417@gmail.com>
2017-01-03 16:03:41 -0500

    first commit for HSP

---
+++ app/controllers/welcome_controller.rb
@@ -0,0 +1,27 @@
+class WelcomeController < ApplicationController
+
+  def index
+
+  end
+
+  def our_story
+
+  end
+
+  def testimonies
+
+  end
+
+  def press
+
+  end
+
+  def gallery
+
+  end
+
+  def contact
+
+  end
+  
+end
---
+++ config/routes.rb
@@ -0,0 +1,11 @@
+Rails.application.routes.draw do
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'welcome#index', as: 'home'
+
+  get '/story'			=>  'welcome#story', as: 'our_story'
+  get '/testimonies'	=> 'welcome#testimonies', as: 'testimonies'
+  get '/press'			=> 'welcome#press', as: 'press'
+  get '/contact'		=> 'welcome#contact', as: 'contact'
+  get '/gallery'		=> 'welcome#gallery', as: 'gallery'
+
+end
