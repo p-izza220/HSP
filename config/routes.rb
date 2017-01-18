23417363e23ef522418a6bf93c912a3ed5b78e8d
Izza <icg417@gmail.com>
2017-01-18 13:53:12 -0500

    added styling, story

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'welcome#index', as: 'home'
 
-  get '/story'			=>  'welcome#story', as: 'our_story'
+  get '/servicerates'	=>  'welcome#servicerates', as: 'services'
   get '/testimonies'	=> 'welcome#testimonies', as: 'testimonies'
   get '/press'			=> 'welcome#press', as: 'press'
   get '/gallery'		=> 'welcome#gallery', as: 'gallery'
